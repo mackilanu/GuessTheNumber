ec95c7e7e66f92c3feb52132b8b22d9450baabbf
Marcus Andersson <mackilanu@gmail.com>
2021-01-04 23:18:52 +0100

    Add project files.

---
+++ GuessTheNumber/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumber
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public void printPerson()
+        {
+            Console.WriteLine("Hello " + name + ". Your age is " + age);
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        
+    }
+}
---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to guess the number. Please enter a guess between 1-100");
+
+            bool correct = false;
+            Random rng = new Random();
+            int random_num = rng.Next(1, 101);
+            int num_attempts = 0;
+
+            for(;;)
+            {
+                num_attempts++;
+                Console.Write("Enter your guess: ");
+                int guess = int.Parse(Console.ReadLine());
+                
+                if(guess == random_num)
+                {
+                    Console.WriteLine("Correct Answer! It took only " + num_attempts +" attempts. Good job :)");
+                    break;
+                }
+                else if(guess > random_num)
+                {
+                    Console.WriteLine("Your guess was too big. Try again!");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess was too small. Try again!");
+                }
+
+            }
+        }
+    }
+}
